a37f609178f7277bff53c6b5238c77e09b77c610
zenarioo <miguelnazariosimoes@gmail.com>
2025-07-14 22:51:18 -0300

    terminando a lista

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
 const taskList = document.getElementById("taskList");
 const taskInput = document.getElementById("taskInput");
 
-function addTask(){
 
+
+function addTask() {
     const taskText = taskInput.value.trim();
 
-    if(taskText !== ""){
+    if (taskText !== "") {
+
+        const maxText = taskText.substring(0, 35);
+
         const li = document.createElement("li");
+
         li.innerHTML = `
-            <span>${taskText}</span>
-            <button class="editButton" onClick="editTask">Editar<button>
-            <button class="deleteButton" onClick="deleteTask">Remover<button>
+
+            <span>${maxText}</span>
+            <button class="editButton" onclick="editTask(this)">Editar</button>
+            <button class="deleteButton" onclick="deleteTask(this)">Remover</button>
         `;
+
         taskList.appendChild(li);
-        taskInput.value = ""
+        taskInput.value = "";
     }
 }
 
 
-function editTask(){
+
+function editTask(button) {
     const li = button.parentElement;
     const span = li.querySelector("span");
-    const newText = prompt("Editar Item:", span.textoContent);
-    
-    if(newText !== null && newText.trim() !== ""){
-        span.textoContent = newText.trim();
+    const newText = prompt("Editar Item:", span.textContent);
 
+    if (newText !== null && newText.trim() !== "") {
+        span.textContent = newText.trim();
     }
+}
+
+
+
+function deleteTask(button) {
+    const li = button.parentElement;
+    taskList.removeChild(li);
 }
